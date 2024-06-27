e0636a57e7b79178a8ce69da919aa85ac26edabd
Salman Arif <salmanarifmir2000@gmail.com>
2024-06-27 18:28:26 +0500

    Improve address screen UI when entering data in fields

--- screens/addressscreen.js
+++ screens/addressscreen.js
@@ -7,8 +7,11 @@ import {
   Pressable,
   Image,
   Alert,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function AddressScreen() {
   const route = useRoute();
@@ -44,54 +47,61 @@ export default function AddressScreen() {
   }, [address]);
 
   return (
-    <View style={StyleSheet.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.addressImage}
-          source={require("../assets/address-bike.png")}
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={80}
+    >
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.addressImage}
+            source={require("../assets/address-bike.png")}
+          />
+        </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          value={address.fullName}
+          onChangeText={(text) => handleChange("fullName", text)}
         />
-      </View>
-      <TextInput
-        style={styles.input}
-        placeholder="Full Name"
-        value={address.fullName}
-        onChangeText={(text) => handleChange("fullName", text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Address"
-        value={address.addressLine}
-        onChangeText={(text) => handleChange("addressLine", text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="City"
-        value={address.city}
-        onChangeText={(text) => handleChange("city", text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email Address"
-        value={address.emailAddress}
-        onChangeText={(text) => handleChange("emailAddress", text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Phone Number"
-        value={address.phoneNumber}
-        onChangeText={(text) => handleChange("phoneNumber", text)}
-      />
-      <Pressable style={styles.doneButton} onPress={handleDone}>
-        <Text style={styles.doneButtonText}>DONE</Text>
-      </Pressable>
-    </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Address"
+          value={address.addressLine}
+          onChangeText={(text) => handleChange("addressLine", text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="City"
+          value={address.city}
+          onChangeText={(text) => handleChange("city", text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email Address"
+          value={address.emailAddress}
+          onChangeText={(text) => handleChange("emailAddress", text)}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Phone Number"
+          value={address.phoneNumber}
+          onChangeText={(text) => handleChange("phoneNumber", text)}
+          keyboardType="phone-pad"
+        />
+        <Pressable style={styles.doneButton} onPress={handleDone}>
+          <Text style={styles.doneButtonText}>DONE</Text>
+        </Pressable>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
   },
   imageContainer: {
     backgroundColor: "#EA9B3F",
